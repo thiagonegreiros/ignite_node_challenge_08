f2651de00945fb35dbc45a2eefbce427237ffa1d
Thiago Negreiros <negreiros.t@gmail.com>
2021-12-11 11:18:43 -0300

    feat: authenticate user test block functions

--- src/tests/users/AuthenticateUserUseCase.spec.ts
+++ src/tests/users/AuthenticateUserUseCase.spec.ts
@@ -16,30 +16,44 @@ describe('Authenticate user', () => {
   })
 
   it('should be able authenticate an user', async () => {
-    const { email }= await createUserUseCase.execute({
+    const { email } = await createUserUseCase.execute({
       name: 'Alan McCormick',
       email: 'co@jid.org',
       password: '123456'
-     })
+    })
 
     const result = await authenticateUserUseCase.execute({ email, password: '123456' });
 
     expect(result).toHaveProperty('token');
-  })
+  });
 
-  // it('should not be able add an user with duplicate email', async () => {
-  //   const { email } = await userRepositoryInMemory.create({
-  //     name: 'Alan McCormick',
-  //     email: 'co@jid.org',
-  //     password: 'U7Z8jNlLZx5xht3Hjm12'
-  //   })
+  it('should not be able authenticate an user that does not exists', async () => {
+    await createUserUseCase.execute({
+      name: 'Matilda Clarke',
+      email: 'gigge@tun.li',
+      password: '123456'
+    })
 
-  //   await expect(
-  //     createUserUseCase.execute({
-  //       name: 'Agnes Farmer',
-  //       email,
-  //       password: 'QlQCsvchpy8uIQcPG6va'
-  //     })
-  //   ).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
-  // })
+    await expect(
+      authenticateUserUseCase.execute({
+        email: 'tomaaso@pubepi.sl',
+        password: '123456'
+      })
+    ).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  });
+
+  it('should not be able authenticate an user with incorrect password', async () => {
+    const { email } = await createUserUseCase.execute({
+      name: 'Christopher Gomez',
+      email: 'gizab@zeghi.aw',
+      password: '123456'
+    })
+
+    await expect(
+      authenticateUserUseCase.execute({
+        email,
+        password: 'incorrect-password'
+      })
+    ).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  });
 })
